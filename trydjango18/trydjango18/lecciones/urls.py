22091a1056608a30c1982bf7e5acb990e97cd11e
martin8767 <martin8767@gmail.com>
2017-01-05 01:37:47 -0300

    mejorado el tema de las lecciones

--- trydjango18/trydjango18/lecciones/urls.py
+++ trydjango18/trydjango18/lecciones/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,7 +7,11 @@ urlpatterns = [
     # Examples:
     url(r'^$', 'lecciones.views.homel', name='homel'),
 
-    url(r'leccion1', 'lecciones.views.leccion1', name='leccion1'),
+    url(r'^leccion1/vocabulario', 'lecciones.views.leccion1vocabulario', name='leccion1v'),
+    url(r'^leccion2/vocabulario', 'lecciones.views.leccion2vocabulario', name='leccion2v'),
+    url(r'^leccion3/vocabulario', 'lecciones.views.leccion3vocabulario', name='leccion3v'),
+    url(r'^leccion4/vocabulario', 'lecciones.views.leccion4vocabulario', name='leccion4v'),
+    
 
 
     # url(r'^blog/', include('blog.urls')),
--- trydjango18/trydjango18/lecciones/views.py
+++ trydjango18/trydjango18/lecciones/views.py
@@ -5,5 +5,29 @@ def homel(request):
     return render(request, "lecciones.html", {})
 
 
-def leccion1(request):
-    return render(request, "lecciones/leccion1.html", {})
+def leccion1vocabulario(request):
+    tipo = {
+        "gg" : "vocabulario",
+    }
+    return render(request, "lecciones/leccion1.html", tipo)
+
+def leccion2vocabulario(request):
+    tipo = {
+        "gg" : "vocabulario",
+    }
+    return render(request, "lecciones/leccion2.html", tipo)
+
+def leccion3vocabulario(request):
+    tipo = {
+        "gg" : "vocabulario",
+    }
+    return render(request, "lecciones/leccion3.html", tipo)
+
+def leccion4vocabulario(request):
+    tipo = {
+        "gg" : "vocabulario",
+    }
+    return render(request, "lecciones/leccion4.html", tipo)
+
+def leccion2(request):
+    return render(request, "lecciones/leccion2.html", {})
